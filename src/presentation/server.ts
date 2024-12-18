0d91c24deabecca58fa2ff61a6097b007bfd596d
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-18 15:45:26 -0500

    114. JSON-Server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -7,6 +7,7 @@ export class Server {
 
 		CronService.createJob('*/5 * * * * *', () => {
 			new CheckService().execute('https://google.com');
+			// new CheckService().execute('http://localhost:3000');
 		});
 	}
 }
