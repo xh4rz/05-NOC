4093ac654e6168b7a680be846b07c0381364da48
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-18 15:54:49 -0500

    115. Inyección de dependencias

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -2,7 +2,15 @@ interface CheckServiceUseCase {
 	execute(url: string): Promise<boolean>;
 }
 
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 export class CheckService implements CheckServiceUseCase {
+	constructor(
+		private readonly successCallback: SuccessCallback,
+		private readonly errorCallback: ErrorCallback
+	) {}
+
 	public async execute(url: string): Promise<boolean> {
 		try {
 			const req = await fetch(url);
@@ -11,11 +19,13 @@ export class CheckService implements CheckServiceUseCase {
 				throw new Error(`Error on check service ${url}`);
 			}
 
-			console.log(`${url} is ok`);
+			this.successCallback();
 
 			return true;
 		} catch (error) {
 			console.log(`${error}`);
+
+			this.errorCallback(`${error}`);
 
 			return false;
 		}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -6,7 +6,12 @@ export class Server {
 		console.log('Server started...');
 
 		CronService.createJob('*/5 * * * * *', () => {
-			new CheckService().execute('https://google.com');
+			const url = 'https://google.com';
+
+			new CheckService(
+				() => console.log(`${url} is ok`),
+				(error) => console.log(error)
+			).execute(url);
 			// new CheckService().execute('http://localhost:3000');
 		});
 	}
