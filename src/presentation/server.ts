ce105c470771dc7d1015b6ecfa16f7a060cc5a58
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-18 15:33:08 -0500

    113. CheckService - UseCase

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from '../domain/use-cases/checks/check-service';
 import { CronService } from './cron/cron-service';
 
 export class Server {
@@ -5,8 +6,7 @@ export class Server {
 		console.log('Server started...');
 
 		CronService.createJob('*/5 * * * * *', () => {
-			const date = new Date();
-			console.log('5 seconds', date);
+			new CheckService().execute('https://google.com');
 		});
 	}
 }
