5285ed7c5cd15dc7abf1caed765379d0d5ddc963
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-19 18:49:14 -0500

    124. FileSystem - SaveLog

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -25,9 +25,20 @@ export class FileSystemDatasource implements LogDatasource {
 		);
 	};
 
-	saveLog(log: LogEntity): Promise<void> {
-		throw new Error('Method not implemented.');
+	async saveLog(newLog: LogEntity): Promise<void> {
+		const logAsJson = `${JSON.stringify(newLog)}\n`;
+
+		fs.appendFileSync(this.allLogsPath, logAsJson);
+
+		if (newLog.level === LogSeverityLevel.low) return;
+
+		if (newLog.level === LogSeverityLevel.medium) {
+			fs.appendFileSync(this.mediumLogsPath, logAsJson);
+		} else {
+			fs.appendFileSync(this.highLogsPath, logAsJson);
+		}
 	}
+
 	getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
 		throw new Error('Method not implemented.');
 	}
