a85f4b257757875fa1232ed15e9b752fa89859e9
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-19 02:11:20 -0500

    123. FileSystem - Datasource

---
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import { LogDatasource } from '../../domain/datasources/log.datasource';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
+
+export class FileSystemDatasource implements LogDatasource {
+	private readonly logPath = 'logs/';
+	private readonly allLogsPath = 'logs/logs-low.log';
+	private readonly mediumLogsPath = 'logs/logs-medium.log';
+	private readonly highLogsPath = 'logs/logs-high.log';
+
+	constructor() {
+		this.createLogsFiles();
+	}
+
+	private createLogsFiles = () => {
+		if (!fs.existsSync(this.logPath)) {
+			fs.mkdirSync(this.logPath);
+		}
+
+		[this.allLogsPath, this.mediumLogsPath, this.highLogsPath].forEach(
+			(path) => {
+				if (fs.existsSync(path)) return;
+				fs.writeFileSync(path, '');
+			}
+		);
+	};
+
+	saveLog(log: LogEntity): Promise<void> {
+		throw new Error('Method not implemented.');
+	}
+	getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+		throw new Error('Method not implemented.');
+	}
+}
