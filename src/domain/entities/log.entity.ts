dc0e3b393af3fa87bd5f878a54d21c0ffbb43563
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-19 19:24:20 -0500

    125. FileSystem - GetLogs

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -9,9 +9,19 @@ export class LogEntity {
 	public message: string;
 	public createdAt: Date;
 
-	constuctor(message: string, level: LogSeverityLevel) {
+	constructor(message: string, level: LogSeverityLevel) {
 		this.message = message;
 		this.level = level;
 		this.createdAt = new Date();
 	}
+
+	static fromJson = (json: string): LogEntity => {
+		const { message, level, createdAt } = JSON.parse(json);
+
+		const log = new LogEntity(message, level);
+
+		log.createdAt = new Date(createdAt);
+
+		return log;
+	};
 }
