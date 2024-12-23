d8ef891b7c684c66db0d45ac5e426e97d6e17767
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-23 15:58:38 -0500

    140. Gmail Keys - Two-factor authentication

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { envs } from '../config/plugins/envs.plugin';
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
@@ -11,15 +12,17 @@ export class Server {
 	public static start() {
 		console.log('Server started...');
 
-		CronService.createJob('*/5 * * * * *', () => {
-			const url = 'https://google.com';
+		console.log(envs.MAILER_SECRET_KEY, envs.MAILER_EMAIL);
 
-			new CheckService(
-				fileSystemLogRepository,
-				() => console.log(`${url} is ok`),
-				(error) => console.log(error)
-			).execute(url);
-			// new CheckService().execute('http://localhost:3000');
-		});
+		// CronService.createJob('*/5 * * * * *', () => {
+		// 	const url = 'https://google.com';
+
+		// 	new CheckService(
+		// 		fileSystemLogRepository,
+		// 		() => console.log(`${url} is ok`),
+		// 		(error) => console.log(error)
+		// 	).execute(url);
+		// 	// new CheckService().execute('http://localhost:3000');
+		// });
 	}
 }
