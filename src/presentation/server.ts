1adf9b9051a24fe1710100f9181b88a60115b47d
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-23 16:26:17 -0500

    141. Nodemailer - Gmail

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,6 @@
-import { envs } from '../config/plugins/envs.plugin';
-import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
-import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
 
 const fileSystemLogRepository = new LogRepositoryImpl(
 	new FileSystemDatasource()
@@ -12,7 +10,18 @@ export class Server {
 	public static start() {
 		console.log('Server started...');
 
-		console.log(envs.MAILER_SECRET_KEY, envs.MAILER_EMAIL);
+		// Mandar email
+		const emailService = new EmailService();
+
+		emailService.sendEmail({
+			to: 'xh4rz98@gmail.com',
+			subject: 'Logs de sistema',
+			htmlBody: `
+				<h3>Logs de sistema - NOC</h3>
+				<p>Lorem valit non veniam ullamco ex eu laborum</p>
+				<p>Ver los adjuntos</p>
+			`
+		});
 
 		// CronService.createJob('*/5 * * * * *', () => {
 		// 	const url = 'https://google.com';
