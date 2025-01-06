d10707ddcc59e499d19a6627531b94f822989705
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-06 03:36:42 -0500

    155. MongoLogDatasource

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -39,4 +39,17 @@ export class LogEntity {
 
 		return log;
 	};
+
+	static fromObject = (object: { [key: string]: any }): LogEntity => {
+		const { message, level, createdAt, origin } = object;
+
+		const log = new LogEntity({
+			message,
+			level,
+			createdAt,
+			origin
+		});
+
+		return log;
+	};
 }
