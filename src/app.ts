87d6dfc912b0e9e369ca3fddc9d25c311e910ca0
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-06 03:12:33 -0500

    154. Crear y leer de Mongo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from './config/plugins/envs.plugin';
-import { MongoDatabase } from './data/mongo';
+import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
 
 (async () => {
@@ -11,6 +11,21 @@ async function main() {
 		mongoUrl: envs.MONGO_URL,
 		dbName: envs.MONGO_DB_NAME
 	});
+
+	// Crear una coleccion
+	// const newLog = await LogModel.create({
+	// 	message: 'Test message desde Mongo',
+	// 	origin: 'App.ts',
+	// 	level: 'low'
+	// });
+
+	// await newLog.save();
+
+	// console.log(newLog);
+
+	// const logs = await LogModel.find();
+
+	// console.log(logs);
 
 	// Server.start();
 	// console.log(envs);
