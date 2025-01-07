90b7fc7aa98690f6d9af2051846f9ea5327fcc93
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-07 02:39:33 -0500

    163. Grabar en Mongo, PosgreSQL y FS simultáneamente

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,16 +1,25 @@
-import { LogSeverityLevel } from '../domain/entities/log.entity';
-import { CheckService } from '../domain/use-cases/checks/check-service';
+import { CheckServiceMultiple } from '../domain/use-cases/checks/check-service-multiple';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
 import { PostgresLogDatasource } from '../infrastructure/datasources/postgres-log.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
+import { CronService } from './cron/cron-service';
+// import { LogSeverityLevel } from '../domain/entities/log.entity';
+// import { CheckService } from '../domain/use-cases/checks/check-service';
 // import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 // import { EmailService } from './email/email.service';
-import { CronService } from './cron/cron-service';
 
-const logRepository = new LogRepositoryImpl(
-	// new FileSystemDatasource()
-	// new MongoLogDatasource()
+// const logRepository = new LogRepositoryImpl(
+// new FileSystemDatasource()
+// new MongoLogDatasource()
+// new PostgresLogDatasource()
+// );
+
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
+
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
+
+const postgresLogRepository = new LogRepositoryImpl(
 	new PostgresLogDatasource()
 );
 
@@ -40,11 +49,22 @@ export class Server {
 
 		// emailService.sendEmailWithFileSystemLogs(['xh4rz98@gmail.com']);
 
-		CronService.createJob('*/5 * * * * *', () => {
-			const url = 'https://google89988asdasd.com';
+		// CronService.createJob('*/5 * * * * *', () => {
+		// 	const url = 'https://google89988asdasd.com';
 
-			new CheckService(
-				logRepository,
+		// 	new CheckService(
+		// 		logRepository,
+		// 		() => console.log(`${url} is ok`),
+		// 		(error) => console.log(error)
+		// 	).execute(url);
+		// 	// new CheckService().execute('http://localhost:3000');
+		// });
+
+		CronService.createJob('*/5 * * * * *', () => {
+			const url = 'https://google.com';
+
+			new CheckServiceMultiple(
+				[fsLogRepository, mongoLogRepository, postgresLogRepository],
 				() => console.log(`${url} is ok`),
 				(error) => console.log(error)
 			).execute(url);
